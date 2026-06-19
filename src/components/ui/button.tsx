348c7ce1b4404230511b5d4e2946ea955fa1d7b4
javier <javiruizar@gmail.com>
2026-06-20 01:54:49 +0200

    improve buttons contrast

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,14 +9,14 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/70",
+        default: "bg-primary text-white hover:bg-primary-dark",
         destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+          "bg-destructive text-white hover:bg-destructive/80",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-background text-secondary hover:bg-accent hover:text-secondary",
         secondary:
-          "bg-secondary text-neutral-100 hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+          "bg-secondary text-white hover:bg-secondary-dark",
+        ghost: "text-secondary hover:bg-accent hover:text-secondary",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
